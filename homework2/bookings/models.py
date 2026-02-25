e7b42104bda022801af61d449da2b0a826e7e2e8
csmith0807 <calebjsmith360@gmail.com>
2026-02-25 19:07:00 +0000

    Clean Django project without virtual environment. Section 3.1-3.3 done

---
+++ homework2/bookings/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+#Movie model
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.IntegerField(help_text="Duration in minutes")
+
+    def __str__self:
+        return self.title
+
+#Seat model 
+class Seat(models.Model):
+    seat_number = models.CharField(max_length=10)
+    is_booked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.seat_number
+
+#Booking model to connect user, movie, and seat
+class Booking(models.Model):
+    # Relationship to movie
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    #String 
+    def __str__(self):
+        return f"{self.user.username} - {self.movie.title} - {self.seat.seat_number}"
---
+++ homework2/bookings/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from .models import Movie, Seat, Booking
+from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
+
+#ViewSet for seat API
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+#ViewSet for booking API
+class BookingViewSet(viewsets.ModelViewSet):
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
+    # Only return bookings belonging to the current user
+    def get_queryset(self):
+        return Booking.objects.filter(user=self.request.user)
+
+    #Assign logged-in user when creating booking
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
