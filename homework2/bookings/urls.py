239750ce4dad73a5e093951e6e4db5d623f81781
csmith0807 <calebjsmith360@gmail.com>
2026-02-26 20:46:36 +0000

    Adding views for booking history, movie list, and seat booking

--- homework2/bookings/urls.py
+++ homework2/bookings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MovieViewSet, SeatViewSet, BookingViewSet
+from . import views
 
 # DefaultRouter automatically creates RESTful routes
 router = DefaultRouter()
@@ -12,5 +13,8 @@ router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
     # All API routes handled here
+    path('movies/', views.movie_list, name='movie_list'),              # Movie list page
+    path('book/<int:movie_id>/', views.book_seat, name='book_seat'),  # Seat booking page
+    path('history/', views.booking_history, name='booking_history'),  # Booking history page
     path('', include(router.urls)),
 ]
--- homework2/bookings/views.py
+++ homework2/bookings/views.py
@@ -3,6 +3,52 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Movie, Seat, Booking
 from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
 from rest_framework import viewsets
+
+#view to display all movies
+def movie_list(request):
+    movies = Movie.objects.all() #get all movies from db
+    return render(request, 'bookings/movie_list.html', {
+        'movies': movies
+    })
+
+#view to book a seat for a movie
+def book_seat(request, movie_id):
+    #get selected movie
+    movie = Movie.objects.get(id=movie_id)
+
+    #return only seats that aren't booked
+    seats = Seat.objects.filter(is_booked=False)
+
+    if request.method == "POST":
+        seat_id = request.POST.get("seat")
+        seat = Seat.objects.get(id=seat_id)
+        #label seat booked
+        seat.is_booked = True
+        seat.save()
+
+        #create booking record
+        Booking.objects.create(
+            movie=movie,
+            seat=seat,
+            user=request.user
+        )
+
+        #return to booking history page
+        return redirect('booking_history')
+
+    return render(request, 'bookings/seat_booking.html', {
+        'movie': movie,
+        'seats': seats
+    })
+
+#view to see current bookings
+def booking_history(request):
+    #get bookings
+    bookings = Booking.objects.filter(user=request.user)
+    return render(request, 'bookings/booking_history.html', {
+        'bookings': bookings
+    })
+
 #ViewSet for seat API
 class SeatViewSet(viewsets.ModelViewSet):
     queryset = Seat.objects.all()
