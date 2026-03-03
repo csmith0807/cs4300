ee2532052c661ed8ab1b0f02676a1d934d8dd8d7
csmith0807 <calebjsmith360@gmail.com>
2026-03-03 03:34:09 +0000

    static change

--- homework2/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/settings.py
@@ -126,9 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+if not DEBUG:
     # Enable the WhiteNoise storage backend
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
