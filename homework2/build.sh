a234d68821abc32683347674ca6c245e6a5522d7
csmith0807 <calebjsmith360@gmail.com>
2026-03-03 03:12:55 +0000

    change admin name

--- homework2/build.sh
+++ homework2/build.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.filter(username='csmith11').exists() or User.objects.create_superuser('csmith11', 'admin@example.com', 'admin123')" | python manage.py shell
